67e7ec85261562d39e0c9c3b11e52e32083db9b0
AlexHorovyi <alexhorovyi@gmail.com>
2022-01-27 22:47:35 +0200

    fadeIn and fadeOut

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,19 @@
 import $ from './lib/lib';
 
-$('button').on('click', function() {
-    $('div').eq(1).toggleClass('active');
-});
+// $('button').on('click', function() {
+//     $('div').eq(1).toggleClass('active');
+// });
 
 $('div').click(function() {
     console.log($(this).index());
 });
 
-console.log($('div').eq(2).find('.some'));
+// console.log($('div').eq(2).find('.some'));
 
 // console.log($('div').eq(2).find('.some'));
 // console.log($('.some').closest('.findme'));
 console.log($('.findme').siblings());
+$('.findme').fadeOut(1800);
 
 
 

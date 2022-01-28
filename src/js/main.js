f1da455d290d771da067d4da426da0c2beba2ced
D:\Vitalik\WORK\vcpkg <git clone https://github.com/Microsoft/vcpkg.git cd vcpkg ./bootstrap-vcpkg.sh ./vcpkg integrate install vcpkg install curl[tool]>
2022-01-28 16:48:00 +0200

    fadeToggle

--- src/js/main.js
+++ src/js/main.js
@@ -4,17 +4,31 @@ import $ from './lib/lib';
 //     $('div').eq(1).toggleClass('active');
 // });
 
-$('div').click(function() {
-    console.log($(this).index());
-});
+// $('div').click(function() {
+//     console.log($(this).index());
+// });
 
 // console.log($('div').eq(2).find('.some'));
 
 // console.log($('div').eq(2).find('.some'));
 // console.log($('.some').closest('.findme'));
-console.log($('.findme').siblings());
-$('.findme').fadeOut(1800);
+// console.log($('.findme').siblings());
+// $('.findme').fadeOut(1800);
 
 
 
 // console.log($('button').html('Hello'));
+
+//44 видео
+
+$('#first').on('click', () => {
+    $('div').eq(1).fadeToggle(800);
+});
+
+$('[data-count="second"]').on('click', () => {
+    $('div').eq(2).fadeToggle(800);
+});
+
+$('button').eq(2).on('click', () => {
+    $('.w-500').fadeToggle(800);
+});

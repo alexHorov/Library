ca2e680e107508d97a8433be8c1f88dd0f31b2cb
AlexHorovyi <alexhorovyi@gmail.com>
2022-01-29 19:42:31 +0200

    modal

--- src/js/main.js
+++ src/js/main.js
@@ -45,3 +45,35 @@ $('.wrap').html(
     `
 );
 $('.dropdown-toggle').dropdown();
+
+$('#trigger').click(() => {
+    $('#trigger').createModal({
+        text: {
+            title: 'Modal title',
+            body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, accusamus eius. Delectus ea praesentium culpa eaque enim temporibus error atque ipsa pariatur. Nihil adipisci rem eaque ipsa excepturi, incidunt beatae!',
+        },
+        btns: {
+            count: 3,
+            settings: [
+                [
+                    'Close', ['btn-danger', 'mr-10'],
+                    true
+                ],
+                [
+                    'Save changes', ['btn-success'],
+                    false,
+                    () => {
+                        alert('Данные сохранены');
+                    }
+                ],
+                [
+                    'Another btn', ['btn-warning', 'ml-10'],
+                    false,
+                    () => {
+                        alert('Hello World');
+                    }
+                ]
+            ]
+        }
+    });
+});
